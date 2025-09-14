650684d6db0a7fd534958e19e736bb8571f5228d
UrielXP <uriel43fonzalida@gmail.com>
2025-09-13 23:58:03 -0300

    Add files via upload

---
+++ TP n° 9 - Javascript/EjJavaScript5.js
@@ -0,0 +1,6 @@
+function suma(x, y) { return x + y; }
+function resta(x, y) { return x - y; }
+function division(x, y) { return x / y; }
+function multiplicacion(x, y) { return x * y; }
+function potencia(x, y) { return Math.pow(x, y); }
+function cuadrado(x) { return x * x; }
